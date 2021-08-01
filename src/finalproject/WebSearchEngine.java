70f396833bbe010159951b57754320616681ef0c
Roxy Palma <leo_9319@yahoo.com>
2021-08-02 02:34:00 +0600

    edit distance sorted

--- src/finalproject/WebSearchEngine.java
+++ src/finalproject/WebSearchEngine.java
@@ -38,6 +38,35 @@ public class WebSearchEngine {
         return matcher.matches();
     }
 	
+	public static int stringCompare(String str1, String str2)
+    {
+  
+        int l1 = str1.length();
+        int l2 = str2.length();
+        int lmin = Math.min(l1, l2);
+  
+        for (int i = 0; i < lmin; i++) {
+            int str1_ch = (int)str1.charAt(i);
+            int str2_ch = (int)str2.charAt(i);
+  
+            if (str1_ch != str2_ch) {
+                return str1_ch - str2_ch;
+            }
+        }
+  
+        // Edge case for strings like
+        // String 1="Geeks" and String 2="Geeksforgeeks"
+        if (l1 != l2) {
+            return l1 - l2;
+        }
+  
+        // If none of the above conditions is true,
+        // it implies both the strings are equal
+        else {
+            return 0;
+        }
+    }
+	
 	//Using Booyer-Moore Algorithm for string searching. This method would return the word count.
 	public static int getWordCount(File filename, String keyword) throws IOException {
 		int count = 0;
@@ -105,7 +134,9 @@ public static void option1() throws Exception{
 
 		String a = suggested.toLowerCase().toString();
 		String b = keys.toLowerCase().toString();
-		if(!(a.equals(b))){
+		a= a.replaceAll("\\s", "");
+		b = b.replaceAll("\\s", "");
+		if((stringCompare(a, b) != 0)){
 			System.out.println("Did you mean: "+suggested);
 			System.out.println();
 		}
